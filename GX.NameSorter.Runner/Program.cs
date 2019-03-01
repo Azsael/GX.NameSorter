983048e300a6e5efb1e5384baf53d18a7e1d3777
Robbie Hameeteman <robbie.hameeteman@amazingco.me>
2019-03-02 08:38:45 +1100

    Add project files.

---
+++ GX.NameSorter.Runner/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GX.NameSorter.Runner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
